195fb41974cfba225af59ca78d08b0f773c14c3b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:59 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "key_vault_certificate_contactses" {
-  description = "All key_vault_certificate_contacts resources"
-  value       = azurerm_key_vault_certificate_contacts.key_vault_certificate_contactses
-}
 output "key_vault_certificate_contactses_contact" {
-  description = "List of contact values across all key_vault_certificate_contactses"
-  value       = [for k, v in azurerm_key_vault_certificate_contacts.key_vault_certificate_contactses : v.contact]
+  description = "Map of contact values across all key_vault_certificate_contactses, keyed the same as var.key_vault_certificate_contactses"
+  value       = { for k, v in azurerm_key_vault_certificate_contacts.key_vault_certificate_contactses : k => v.contact }
 }
 output "key_vault_certificate_contactses_key_vault_id" {
-  description = "List of key_vault_id values across all key_vault_certificate_contactses"
-  value       = [for k, v in azurerm_key_vault_certificate_contacts.key_vault_certificate_contactses : v.key_vault_id]
+  description = "Map of key_vault_id values across all key_vault_certificate_contactses, keyed the same as var.key_vault_certificate_contactses"
+  value       = { for k, v in azurerm_key_vault_certificate_contacts.key_vault_certificate_contactses : k => v.key_vault_id }
 }
 
